835c6ed53b77000ea6edc48329e59557a9437b19
Dontores <kiefercg2014@gmail.com>
2017-08-02 10:26:30 -0400

    se comento la parte de inscribir materias para una mejor comprension

--- app/Http/Controllers/TeacherSubjects.php
+++ app/Http/Controllers/TeacherSubjects.php
@@ -74,37 +74,53 @@ class TeacherSubjects extends Controller
      */
     public function update(Request $request, $id)
     {
+        //se declara una variable para guardar todas las materias que pertenescan a ese id de profesor
         $subjectsRegistered = DB::table('teachersubjects')
             ->where('teachersubjects.users_id','=',$id)
             ->get();
+        //se declara una variable arreglo para guardar los id de las materias que sean iguales a las ya registradas
         $sameSubject = [];    
 
-        if(count($request->subject_selected) > 0){   
+        if(count($request->subject_selected) > 0){  //si se selecciono una materia entonces entra a este apartado.
+        //mientras x sea menol que la cantidad de materias encontradas de este usuario has lo siguiente
         for($x=0;$x<count($subjectsRegistered);$x++){
             
+            //otro for para mientras y sea menol que las materias seleccionadas has lo siguiente
             for($y=0;$y<count($request->subject_selected);$y++){
+
+            /*al ser un for anidado hara el loop con la materiaregistrada[x] empezando de 0
+            y verificara que en cada materia seleccionada no halla un id que sea igual a la ya registrada
+            si encuentra un id igual entonces has lo siguiente*/    
+
             if($subjectsRegistered[$x]->subjects_id == $request->subject_selected[$y]){
-                
+               
+                /*entrame dentro de la variable arreglo que declaramos arriba el id de la materia
+                que sea igual a la materia registrada*/
                 array_push($sameSubject,$request->subject_selected[$y]);
                 }
             }
-            }
+        }
+        /*utilizamos el metodo array_diff el cual elimina posiciones de un arreglo
+        entonces al arreglo de materias seleccionadas le eliminamos el arreglo de materias iguales
+        esto eliminara todos los id que ya esten registrados dentro de los seleccionados , dejandonos
+        solo los ids que no esten registrados*/
             $newSubjects = array_diff($request->subject_selected, $sameSubject);
-            sort($newSubjects);
+            sort($newSubjects);//con sor ordenamos el arreglo para que no queden posiciones vacias y empieze desde 0;
+            
+            /*hacemos otro for mientras x sea menol que la cantidad de materias no registradas
+            que se valla sumando*/
             for($x=0;$x<count($newSubjects);$x++){
-            /*$subjects = new teachersubjects;
-            $subjects->users_id = $id;
-            $subjects->subjects_id = $newSubjects[$x];
-            $subjects->save();*/
+            
+            //aqui creamos un registro por cada materia no registrada que se selecciono
             DB::table('teachersubjects')->insert(
             ['users_id' => $id, 
             'subjects_id' => $newSubjects[$x]]
             );
-            }
+            }//se envia el mensaje de que se registro correctamente y se vuelve al apartado de materias
             session::flash('message', 'Materias registradas...');
             return redirect('/teachers/'.$id);
             }
-            else{
+            else{//aqui si no se seleciono una materia entonces hace lo mismo de arriba pero con el mensaje no se han hecho cambios
             session::flash('message', 'no se han hecho cambios...');
             return redirect('/teachers/'.$id); 
             }
